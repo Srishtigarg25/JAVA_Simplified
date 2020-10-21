7b88247966a6f7036682ad78e801f46a6828e335
SRISHTI GARG <51504143+Srishtigarg25@users.noreply.github.com>
2020-10-21 23:40:02 +0530

    Blank space pattern

---
+++ pattern7.java
@@ -0,0 +1,30 @@
+package day2;
+
+public class pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int row;
+		int n;
+		while (row <= n) {
+			int csp = 1;
+			while (csp <= n - row) {
+				System.out.print(" ");
+			}
+
+		
+		// inc value
+		int cst = 1;
+		while (cst <= row) {
+			cst++;
+
+		}
+		// dec value
+		int cst = 1;
+		while (cst < row) {
+			row++;
+		}
+
+	}
+	}
+
